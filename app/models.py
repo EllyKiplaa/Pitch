74cefd1ed3f0f998c801c89ab34faadcad81cc31
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-14 01:36:05 +0300

    Add the category class

---
+++ app/models.py
@@ -0,0 +1,18 @@
+class Category:
+    '''
+    # Category class to define category Objects
+    # '''
+    # def ___init__(self,id,name,description):
+    #     self.id = id
+    #     self.name = name
+    #     self.description= description
+
+    # def save_category(self):
+    #     '''
+    #     Function that saves a category
+    #     '''
+       
+    # def get_categories(cls):
+
+    #     Category.all_categories.append()
+    #     return categories
