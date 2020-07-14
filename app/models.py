b2c414bc3e19a416f11f3678c7ed851e480289d1
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-14 20:02:16 +0300

    add removed the class category

--- app/models.py
+++ app/models.py
@@ -1,32 +1,17 @@
 from . import db
-class Categories:
-    '''
-    # Category class to define category Objects
-    # '''
-    # def ___init__(self,id,name,description):
-    #     self.id = id
-    #     self.name = name
-    #     self.description= description
-
-    # def save_category(self):
-    #     '''
-    #     Function that saves a category
-    #     '''
-       
-    # def get_categories(cls):
-
-    #     Category.all_categories.append()
-    #     return categories
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
 
     def __repr__(self):
         return f'User {self.username}'
 
-class Role(db.Model):
+class Post(db.Model):
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer,primary_key = True)
