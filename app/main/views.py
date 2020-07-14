cabb3ff38a6bf248b7682d680ac2949f0fe51fee
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-14 20:04:50 +0300

    Fix a bug

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for
-from app import main
-from ..models import Category   
+from . import main
+from ..models import User
 
 
 
@@ -11,5 +11,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = "Hello world "
-    return render_template('index.html', message = message)
+    User = User.get_category()
+
+    title = 'Pitch your idea for you a so brilliant'
+
+    return render_template('index.html', category = User)
+
