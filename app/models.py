25d21521959da96ac24586c1cb68abc85398d05f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-14 17:16:58 +0300

    Add the role class of the users

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-class Category:
+class Categories:
     '''
     # Category class to define category Objects
     # '''
@@ -25,3 +25,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
