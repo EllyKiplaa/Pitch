a4024d18b7a50071c64ee446fe532d523481d70b
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-15 20:25:04 +0300

    Import the Pitch user

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 import manage
-from app.models import User
+from app.models import User,Pitch
 
 # Creating app instance
 app = create_app('development')
