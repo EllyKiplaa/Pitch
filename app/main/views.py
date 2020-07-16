638b762125cb3949110e458fb78cbea5c62bd90a
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-16 13:28:51 +0300

    add the rout for the new route of the pitch

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,redirect,url_for
 from . import main
 from ..models import User
+from flask_login import login_required
 
 
 
@@ -15,5 +16,23 @@ def index():
 
     title = 'Pitch your idea for you a so brilliant'
 
-    return render_template('index.html', category = User)
+    return render_template('index.html')
+
+@main.route('/pitch/new',methods=["GET","POST"])
+def new_pitch():
+    
+    form = "form"
+    title = ''
+    pitches=""
+
+    return render_template('pitch.html', title=title, form=form, pitch_list=pitches)
+
+@main.route('/comment/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_comment():
+
+    comment_form="comment_form"
+    comments = ''
+    return render_template('form.html', comment_form=comment_form, comment_list=comments)
+
 
