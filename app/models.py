2de25bc3ce4af0bb96078969e95ec98402319489
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-16 13:45:47 +0300

    add property that saves password

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
 
 
@@ -7,11 +8,23 @@ class User(db.Model):
     username = db.Column(db.String(255))
     email =db.Column(db.String(255))
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    pass_secure = db.Column(db.String(255))
-   
+    pass_secure  = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'User {self.username}'
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+def __repr__(self):
+    return f'User {self.username}'
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
