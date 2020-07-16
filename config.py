18ac606b34bb720ad5cdb068da6535304d864936
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-17 01:22:58 +0300

    Add the configurations of my app

--- config.py
+++ config.py
@@ -1,16 +1,30 @@
 import os
 
 class Config:
+    '''
+    General configuration parent class
+    '''
     
 
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://elly:Access2020@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://elly:Access2020@localhost/pitch'
 
-
+ 
 class ProdConfig(Config):
-    pass
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
 
 
 class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+    Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://elly:Access2020@localhost/pitch'
     DEBUG = True
 
 config_options = {
