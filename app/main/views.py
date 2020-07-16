004e4045f9db77bc8b2a333cdc184c92b5b31e5b
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-16 20:28:00 +0300

    fix a bug

--- app/main/views.py
+++ app/main/views.py
@@ -12,8 +12,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    User = User.get_category()
-
+   
     title = 'Pitch your idea for you a so brilliant'
 
     return render_template('index.html')
